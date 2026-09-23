3e6b3c4ca13dd9ec8a660c5b60bdb329e455d66a
Hoplan <104734909+Hxplan@users.noreply.github.com>
2026-09-23 13:56:21 +0200

    animal + Main

--- Veterin'air/Veterin'air/Animal.cs
+++ Veterin'air/Veterin'air/Animal.cs
@@ -8,5 +8,116 @@ namespace Veterin_air
 {
     internal class Animal
     {
+
+        #region Champs
+        private string _nom;
+        private int _age;
+        private float _poids;
+        private string _numeroPuce;
+        private Espece _espece;
+        #endregion
+
+        #region Propriétés
+
+        public string Nom
+        {
+            get { return _nom; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Le nom ne peut pas être vide ou nul.");
+                }
+                _nom = value;
+            }
+            
+        }
+
+        public string age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 0 || value > 50)
+                {
+                    throw new ArgumentException("L'âge doit être compris entre 0 et 50.");
+                }
+                _age = value;
+               
+
+            }
+        }
+
+        public float poids
+        {
+            get { return _poids; }
+            set
+            {
+                if (value < 0 || value > 1000)
+                {
+                    throw new ArgumentException("Le poids doit être compris entre 0 et 1000.");
+                }
+                _poids = value;
+            }
+        }
+
+        public string numeroPuce
+        {
+            get { return _numeroPuce; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length != 15 || !value.All(char.IsLetterOrDigit))
+                {
+                    throw new ArgumentException("Le numéro de puce doit être composé de 15 caractères.");
+                }
+            }
+        }
+
+        public Espece espece
+        {
+            get { return _espece; }
+            set { _espece = value; }
+        }
+
+        #endregion
+
+        #region Constructeurs
+
+        public Animal()
+        {
+            _nom = "Inconnu";
+            _age = 0;
+            _poids = 0.0f;
+            _numeroPuce = "000000000000000";
+            _espece = Espece.Chien;
+        }
+
+        public Animal(string nom, int age, float poids, string numeroPuce, Espece espece)
+        {
+            Nom = nom;
+            this.age = age;
+            this.poids = poids;
+            this.numeroPuce = numeroPuce;
+            this.espece = espece;
+        }
+
+        #endregion
+
+        #region Methodes
+        public void Vieillir()
+        {
+            age++;
+        }
+
+        public void Peser(float poids)
+        {
+
+            poids += poids;
+            if (poids < 0)
+            {
+                throw new ArgumentException("Le poids ne peut pas être négatif.");
+            }
+
+        }
     }
 }
--- Veterin'air/Veterin'air/Main.cs
+++ Veterin'air/Veterin'air/Main.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Veterin_air
 {
-    public class Class1
+    public class Main
     {
     }
 }
