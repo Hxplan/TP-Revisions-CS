f97512b40ab905f20049568c3317605086633fac
Hoplan <104734909+Hxplan@users.noreply.github.com>
2026-09-23 14:17:52 +0200

    ref

--- Veterin'air/UIVeterin'air/Form1.cs
+++ Veterin'air/UIVeterin'air/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace UIVeterin_air
 {
--- Veterin'air/UIVeterin'air/Program.cs
+++ Veterin'air/UIVeterin'air/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UIVeterin_air
 {
